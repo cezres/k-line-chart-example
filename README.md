6cc30fddae5c03f35461fd6217bc153628a5cb7a
晨风 <cezres@163.com>
2024-02-18 23:42:08 +0800

    Test performance

--- README.md
+++ README.md
@@ -18,3 +18,23 @@ macOS|Web|iOS
 :-|:-|:-
 ![img](https://github.com/cezres/K-line-chart-example/blob/main/assets/desktop.png) | ![img](https://github.com/cezres/K-line-chart-example/blob/main/assets/web.png) | ![img](https://github.com/cezres/K-line-chart-example/blob/main/assets/mobile.png)
 
+
+
+-----
+
+
+**在后台执行计算任务并大量传递数据时，复用 Isolate 并使用 TransferableTypedData 传递数据会有更好的性能**，[测试方法](https://github.com/cezres/k-line-chart-example/blob/main/test/widget_test.dart#L19)。
+
+
+
+```bash
+1. Use compute:
+time:2952	result:150005000
+✓ Test performance of Isolate 1. Use compute
+2. Reuse Isolate and use SendPort to send data:
+time:2146	result:150005000
+✓ Test performance of Isolate 2. Reuse Isolate and use SendPort to send data
+3. Send TransferableTypedData:
+time:1832	result:150005000
+✓ Test performance of Isolate 3. Send TransferableTypedData
+```
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,13 +6,129 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'dart:async';
+import 'dart:isolate';
+import 'dart:typed_data';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:k_line_chart_example/utils/merge_sorted_arrays.dart';
 
 void main() {
+  group('Test performance of Isolate', () {
+    const count = 10000;
+    final initialValue = List.generate(10000, (index) => index + 1);
+    List<int> callback(List<int> value) {
+      return value.map((e) => e + 1).toList();
+    }
+
+    test('1. Use compute', () async {
+      final stopwatch = Stopwatch()..start();
+      var value = initialValue;
+      for (var i = 0; i < count; i++) {
+        value = await compute(callback, value);
+      }
+      stopwatch.stop();
+      final result =
+          value.fold(0, (previousValue, element) => previousValue + element);
+      debugPrint(
+          "1. Use compute:\ntime:${stopwatch.elapsedMilliseconds}\tresult:$result");
+    });
+
+    test('2. Reuse Isolate and use SendPort to send data', () async {
+      final stopwatch = Stopwatch()..start();
+      final receivePort = ReceivePort();
+      var value = initialValue;
+      var index = 0;
+      late SendPort sendPort;
+
+      compute(
+        (message) async {
+          final sendPort = message;
+          final receivePort = ReceivePort();
+          sendPort.send(receivePort.sendPort);
+          await for (var element in receivePort) {
+            if (element == 0) {
+              return;
+            }
+            sendPort.send(callback(element));
+          }
+        },
+        receivePort.sendPort,
+      );
+      await for (var element in receivePort) {
+        if (element is SendPort) {
+          sendPort = element;
+          sendPort.send(value);
+        } else if (element is List<int>) {
+          if (index == count) {
+            sendPort.send(0); // exit
+            receivePort.close();
+            value = element;
+          } else {
+            sendPort.send(element);
+          }
+        }
+        index += 1;
+      }
+      stopwatch.stop();
+      final result =
+          value.fold(0, (previousValue, element) => previousValue + element);
+      debugPrint(
+          "2. Reuse Isolate and use SendPort to send data:\ntime:${stopwatch.elapsedMilliseconds}\tresult:$result");
+    });
+
+    test('3. Send TransferableTypedData', () async {
+      final stopwatch = Stopwatch()..start();
+      final receivePort = ReceivePort();
+      var value = Uint32List.fromList(initialValue);
+      var index = 0;
+      late SendPort sendPort;
+
+      compute(
+        (message) async {
+          final sendPort = message;
+          final receivePort = ReceivePort();
+          sendPort.send(receivePort.sendPort);
+          await for (var element in receivePort) {
+            if (element == 0) {
+              return;
+            }
+            sendPort.send(TransferableTypedData.fromList([
+              Uint32List.fromList(callback((element as TransferableTypedData)
+                  .materialize()
+                  .asUint32List()))
+            ]));
+          }
+        },
+        receivePort.sendPort,
+      );
+      await for (var element in receivePort) {
+        if (element is SendPort) {
+          sendPort = element;
+          sendPort.send(TransferableTypedData.fromList([value]));
+          index += 1;
+        } else if (element is TransferableTypedData) {
+          if (index == count) {
+            sendPort.send(0);
+            receivePort.close();
+            value = element.materialize().asUint32List();
+          } else {
+            sendPort.send(TransferableTypedData.fromList(
+                [element.materialize().asUint32List()]));
+          }
+          index += 1;
+        }
+      }
+      stopwatch.stop();
+      final result =
+          value.fold(0, (previousValue, element) => previousValue + element);
+      debugPrint(
+          "3. Send TransferableTypedData:\ntime:${stopwatch.elapsedMilliseconds}\tresult:$result");
+    });
+  });
+
   test('list', () {
     /// 合并两个正序并且数据连续的数组
     /// 优先保留[list1]的数据
